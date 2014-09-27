96a1f5a23535b82d046f7319abc1b4066b00431d
Philip Hayes <philiphayes9@gmail.com>
2014-09-27 14:55:29 -0700

    Build initial static React Components

--- static/js/app.jsx
+++ static/js/app.jsx
@@ -2,6 +2,19 @@
 
 (function() {
 
+  var sites = [
+    {
+      name: 'BearFacts',
+      url: 'http://bearfacts.berkeley.edu',
+      tags: ['financial aid', 'registration', 'general']
+    },
+    {
+      name: 'Financial Aid',
+      url: 'http://myfinaid.berkeley.edu',
+      tags: ['financial aid']
+    },
+  ];
+
   var CategoryList = React.createClass({
     render: function() {
       var categories = this.props.data.map(function(category) {
@@ -18,6 +31,29 @@
     }
   });
 
+  var TilesComponent = React.createClass({
+    render: function() {
+      var tiles = this.props.data.map(function(datum) {
+        return (
+          <div className="col-sm-4 col-lg-4 col-md-4">
+            <a href="http://google.com/">
+              <div className="thumbnail">
+                <img src="http://placehold.it/300x300" alt="" />
+                <h4 className="caption">{ datum.name }</h4>
+              </div>
+            </a>
+          </div>
+        );
+      });
+
+      return (
+        <div className="row">
+          { tiles }
+        </div>
+      );
+    }
+  });
+
   var AppComponent = React.createClass({
 
     render: function() {
@@ -29,121 +65,7 @@
         </div>
 
         <div className="col-md-9">
-
-          <div className="row">
-
-            <div className="col-sm-4 col-lg-4 col-md-4">
-              <div className="thumbnail">
-                <img src="http://placehold.it/320x150" alt="" />
-                <div className="caption">
-                  <h4 className="pull-right">$24.99</h4>
-                  <h4><a href="#">First Product</a>
-                  </h4>
-                  <p>See more snippets like this online store item at <a target="_blank" href="http://www.bootsnipp.com">Bootsnipp - http://bootsnipp.com</a>.</p>
-                </div>
-                <div className="ratings">
-                  <p className="pull-right">15 reviews</p>
-                  <p>
-                    <span className="glyphicon glyphicon-star"></span>
-                    <span className="glyphicon glyphicon-star"></span>
-                    <span className="glyphicon glyphicon-star"></span>
-                    <span className="glyphicon glyphicon-star"></span>
-                    <span className="glyphicon glyphicon-star"></span>
-                  </p>
-                </div>
-              </div>
-            </div>
-
-            <div className="col-sm-4 col-lg-4 col-md-4">
-              <div className="thumbnail">
-                <img src="http://placehold.it/320x150" alt="" />
-                <div className="caption">
-                  <h4 className="pull-right">$64.99</h4>
-                  <h4><a href="#">Second Product</a>
-                  </h4>
-                  <p>This is a short description. Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
-                </div>
-                <div className="ratings">
-                  <p className="pull-right">12 reviews</p>
-                  <p>
-                    <span className="glyphicon glyphicon-star"></span>
-                    <span className="glyphicon glyphicon-star"></span>
-                    <span className="glyphicon glyphicon-star"></span>
-                    <span className="glyphicon glyphicon-star"></span>
-                    <span className="glyphicon glyphicon-star-empty"></span>
-                  </p>
-                </div>
-              </div>
-            </div>
-
-            <div className="col-sm-4 col-lg-4 col-md-4">
-              <div className="thumbnail">
-                <img src="http://placehold.it/320x150" alt="" />
-                <div className="caption">
-                  <h4 className="pull-right">$74.99</h4>
-                  <h4><a href="#">Third Product</a>
-                  </h4>
-                  <p>This is a short description. Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
-                </div>
-                <div className="ratings">
-                  <p className="pull-right">31 reviews</p>
-                  <p>
-                    <span className="glyphicon glyphicon-star"></span>
-                    <span className="glyphicon glyphicon-star"></span>
-                    <span className="glyphicon glyphicon-star"></span>
-                    <span className="glyphicon glyphicon-star"></span>
-                    <span className="glyphicon glyphicon-star-empty"></span>
-                  </p>
-                </div>
-              </div>
-            </div>
-
-            <div className="col-sm-4 col-lg-4 col-md-4">
-              <div className="thumbnail">
-                <img src="http://placehold.it/320x150" alt="" />
-                <div className="caption">
-                  <h4 className="pull-right">$84.99</h4>
-                  <h4><a href="#">Fourth Product</a>
-                  </h4>
-                  <p>This is a short description. Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
-                </div>
-                <div className="ratings">
-                  <p className="pull-right">6 reviews</p>
-                  <p>
-                    <span className="glyphicon glyphicon-star"></span>
-                    <span className="glyphicon glyphicon-star"></span>
-                    <span className="glyphicon glyphicon-star"></span>
-                    <span className="glyphicon glyphicon-star-empty"></span>
-                    <span className="glyphicon glyphicon-star-empty"></span>
-                  </p>
-                </div>
-              </div>
-            </div>
-
-            <div className="col-sm-4 col-lg-4 col-md-4">
-              <div className="thumbnail">
-                <img src="http://placehold.it/320x150" alt="" />
-                <div className="caption">
-                  <h4 className="pull-right">$94.99</h4>
-                  <h4><a href="#">Fifth Product</a>
-                  </h4>
-                  <p>This is a short description. Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
-                </div>
-                <div className="ratings">
-                  <p className="pull-right">18 reviews</p>
-                  <p>
-                    <span className="glyphicon glyphicon-star"></span>
-                    <span className="glyphicon glyphicon-star"></span>
-                    <span className="glyphicon glyphicon-star"></span>
-                    <span className="glyphicon glyphicon-star"></span>
-                    <span className="glyphicon glyphicon-star-empty"></span>
-                  </p>
-                </div>
-              </div>
-            </div>
-
-          </div>
-
+          <TilesComponent data={ [ {name:'Foo Bar'}, {name:'Baz Quxx'}] } />
         </div>
 
       </div>
@@ -152,22 +74,4 @@
   });
 
   React.renderComponent(<AppComponent />, document.getElementById('react-binding'));
-
-
-  var sites = [
-    {
-      name: 'BearFacts',
-      url: 'http://bearfacts.berkeley.edu',
-      tags: ['financial aid', 'registration', 'general']
-    },
-    {
-      name: 'Financial Aid',
-      url: 'http://myfinaid.berkeley.edu',
-      tags: ['financial aid']
-    },
-  ];
-
-
-
-
 })()
